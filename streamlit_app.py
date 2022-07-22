c59bea6198101b1e9a176e0a0d74515e63495675
GloSnowflake <109616265+GloSnowflake@users.noreply.github.com>
2022-07-22 15:58:27 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,14 +51,27 @@ streamlit.stop()
 
 #import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute("SELECT * from fruit_load_list")
 #my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
+#my_data_rows = my_cur.fetchall()
 #streamlit.dataframe(my_data_row)
-streamlit.dataframe(my_data_rows)
+#streamlit.dataframe(my_data_rows)
+
+streamlit.header("The fruit load list contains:")
+#snowflake-related functions
+def get_fruit_load_list()
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall()
+#Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)   
+                       
+
 
 
 
