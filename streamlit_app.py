beb25fc9db0e34a1c6015c607829f0d359753bc4
GloSnowflake <109616265+GloSnowflake@users.noreply.github.com>
2022-07-22 15:37:50 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,9 +26,9 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
 if not fruit_choice:
-  streamlit.write('The user entered ', fruit_choice)
+  steamlit.error("Please select a fruit to get information")
 else:
   #import requests
   #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
@@ -37,9 +37,10 @@ else:
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   # write your own comment - what does this do?
   streamlit.dataframe(fruityvice_normalized)
-except URLError as e: streamlit.error()
+except URLError as e: 
+      streamlit.error()
 
-
+streamlit.write('The user entered ', fruit_choice)
 
 #don't run anything past here while we troubleshoot
 streamlit.stop()
