7732b7d24fd383d08b56ded1ff8913039dc28dfd
GloSnowflake <109616265+GloSnowflake@users.noreply.github.com>
2022-07-22 15:28:16 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -33,7 +38,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 #don't run anything past here while we troubleshoot
 streamlit.stop()
-from urllib.error import URLError
+
 
 #import snowflake.connector
 
